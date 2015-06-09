7f2a0b539424e12958b007cf7a77cfd31313b5c2
Alexander078 <alexguevara078@gmail.com>
2015-06-08 23:20:51 -0600

    probando

--- Source/Main.java
+++ Source/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         
-        JOptionPane.showMessageDialog(null, "SISTEMA DE INFORMACION GERENCIAL");
+        JOptionPane.showMessageDialog(null, "HOLA MUNDO");
         
     }
     
