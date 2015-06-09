781af0db2a2847a50a4810f8599bbe0da6949a61
hitachi <hitachi@zerberus>
2015-06-08 23:11:45 -0600

    modificando mensaje

--- Source/Main.java
+++ Source/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         
-        JOptionPane.showMessageDialog(null, "probando GIT");
+        JOptionPane.showMessageDialog(null, "SISTEMA DE INFORMACION GERENCIAL");
         
     }
     
