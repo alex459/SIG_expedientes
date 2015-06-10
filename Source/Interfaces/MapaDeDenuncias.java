ca5efd18bfe8de8f00eb9b3070297dc99b303238
Kevin <Kevin@KevinPC>
2015-06-10 13:47:57 -0600

    modificaciones 1 de la tarde

--- Source/Interfaces/MapaDeDenuncias.java
+++ Source/Interfaces/MapaDeDenuncias.java
@@ -28,7 +28,8 @@ public class MapaDeDenuncias extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Menú Principal");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
