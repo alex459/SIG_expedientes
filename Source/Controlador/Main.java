50ec0ec3b88a65d653890ade8e2ef5496317a21c
Kevin <Kevin@KevinPC>
2015-06-10 01:19:09 -0600

    Main

---
+++ Source/Controlador/Main.java
@@ -0,0 +1,16 @@
+package Controlador;
+
+
+import Interfaces.LoginUser;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       
+        ControladorClases cc = new ControladorClases();
+        cc.AbrirLogin();
+        
+    }
+
+}
