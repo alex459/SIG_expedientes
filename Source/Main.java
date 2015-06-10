9841274db04393f5cdf57e41df494c0258fc841f
Kevin <Kevin@KevinPC>
2015-06-09 23:25:03 -0600

    Abrir Login

--- Source/Main.java
+++ Source/Main.java
@@ -1,13 +1,18 @@
 
+import Interfaces.LoginUser;
 import javax.swing.JOptionPane;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        
-        JOptionPane.showMessageDialog(null, "HOLA MUNDO");
-        
+       
+     
+    }
+    
+     void AbrirLogin(){
+           LoginUser Lgu = new LoginUser();
+        Lgu.setVisible(true);
     }
     
 }
