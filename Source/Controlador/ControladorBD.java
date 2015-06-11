30a5451a209d9d6d8fb03beb396fa6ec0dd488e1
jbeltran2 <jbeltran2@Jbeltran>
2015-06-11 14:54:16 -0600

    Conexion con la base de datos

--- Source/Controlador/ControladorBD.java
+++ Source/Controlador/ControladorBD.java
@@ -1,5 +1,9 @@
 package Controlador;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +15,31 @@ package Controlador;
  * @author Kevin
  */
 public class ControladorBD {
+    private Connection con;
+    protected String serverName = "Jbeltran\\sqlexpress";
+    protected String tcpip = "49157";
+    
+    public void AbrirConexion(){
+        
+        
+            try{
+                
+                serverName = JOptionPane.showInputDialog("Ingrese su ServerName");
+                tcpip = JOptionPane.showInputDialog("Ingrese su TCPIP");
+                String url = "jdbc:sqlserver://"+serverName+":"+tcpip+";database=Expedientes;integratedSecurity=true;";
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+                con = DriverManager.getConnection(url);
+                JOptionPane.showMessageDialog(null, "Conexion con la base de datos establecida");
+                
+                //String url = "jdbc:sqlserver://Jbeltran\\sqlexpress:49157;database=Expedientes;integratedSecurity=true;";
+                //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+                //con = DriverManager.getConnection(url);
+                //System.out.println("Conexion a la BD");
+                
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null, "ERRROR: "+e.getMessage());
+            }
+        
+    }
     
 }
--- Source/Controlador/Main.java
+++ Source/Controlador/Main.java
@@ -8,8 +8,12 @@ public class Main {
 
     public static void main(String[] args) {
        
+        ControladorBD bd = new ControladorBD();
+        bd.AbrirConexion();
+        
         ControladorClases cc = new ControladorClases();
         cc.AbrirLogin();
+        
         
     }
 
