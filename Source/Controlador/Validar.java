8968821deb183f36fed73752414b1429b3832a5f
hitachi <hitachi@zerberus>
2015-06-14 17:20:31 -0600

    clase de validaciones. validar texto

---
+++ Source/Controlador/Validar.java
@@ -0,0 +1,21 @@
+
+
+package Controlador;
+
+
+public class Validar {
+    
+    public boolean  ValidarTexto(String texto){
+    for(int i=0; i<texto.length(); i++){
+        if(!Character.toString(texto.charAt(i)).contains("ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz")){
+            return false;
+            
+        }
+    }
+        
+    return true;
+    }
+    
+
+    
+}
