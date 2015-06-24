0faaccb757e5a8f419f931fecfe445a252647211
hitachi <hitachi@zerberus>
2015-06-24 14:16:47 -0600

    valida fechas creado

--- Source/Controlador/Validar.java
+++ Source/Controlador/Validar.java
@@ -2,6 +2,8 @@
 
 package Controlador;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JOptionPane;
 
 
@@ -46,6 +48,26 @@ public class Validar {
         }
     }  
     return true;
+    }
+        
+    public boolean validarFecha2MayorQueFecha1(Date f1, Date f2){
+        SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+        boolean validarFechas=false;
+        float anio1=Float.parseFloat(f.format(f1).toString().substring(0, 4));
+        float anio2=Float.parseFloat(f.format(f2).toString().substring(0, 4));
+        float mes1=Float.parseFloat(f.format(f1).toString().substring(5, 7));
+        float mes2=Float.parseFloat(f.format(f2).toString().substring(5, 7));
+        float dia1=Float.parseFloat(f.format(f1).toString().substring(8, 10));
+        float dia2=Float.parseFloat(f.format(f2).toString().substring(8, 10));
+        
+        
+        if((anio1*3)+(mes1*2)+dia1 < (anio2*3)+(mes2*2)+dia2)
+            return true;
+        else
+            return false;
+        
+        
+        
     }
     
     
